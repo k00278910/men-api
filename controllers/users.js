7d74b0baa4d564cb82881bcbee51ec46e736cee5
k00278910 <k00278910@student.lit.ie>
2024-01-06 12:24:21 +0000

    (1) Setup Route for create user. (2) Adds create user function in controller (3) Setup Middleware to parse out the body to gain access to request variables (4) Use POSTMAN to send request to api endpoints (5) save request data to database via CreateUser function.

--- controllers/users.js
+++ controllers/users.js
@@ -10,21 +10,34 @@ exports.index = (req,res)=>{
 exports.listAll = (req,res)=>{
     res.send('List all Users');
 };
-
-exports.createUser = (req,res)=>{
+/* asynch function returns a promise
+ a Promise is an object that will 
+produce a single value some time in 
+the future*/
+exports.createUser = async(req,res)=>{
     const user = new User({
         name:req.body.name,
             title:req.body.title,
             bio:req.body.bio,
     });
 
-    // Save to DB
-    // Use promise
-    user.save().then((data)=>{
-        // 200 for ok status
-    res.status(200).json(data);
-    }).catch((err)=>{
-        // 200 for ok status
-    res.status(400).json({msg:err});
-    }); 
+
+    // // Save to DB
+    // // Use promise
+    // user.save().then((data)=>{
+    //     // 200 for ok status
+    // res.status(200).json(data);
+    // }).catch((err)=>{
+    //     // 200 for ok status
+    // res.status(400).json({msg:err});
+    // }); 
+    try{
+        const createUser = await user.save();
+        res.status(201).json(user);
+    }
+    catch(err){
+        res.status(400).json({msg:err});
+    }
+        
+    
 };
